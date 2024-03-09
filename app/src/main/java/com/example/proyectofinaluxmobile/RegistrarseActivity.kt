258d6e47e3e821dedd1aef9dfdea3b6d8fe5a2f1
IngSneyderAmado <s.amadov@uniandes.edu.co>
2024-03-08 22:02:35 -0500

    se agrega la interacion entre pantallas

--- app/src/main/java/com/example/proyectofinaluxmobile/RegistrarseActivity.kt
+++ app/src/main/java/com/example/proyectofinaluxmobile/RegistrarseActivity.kt
@@ -2,10 +2,28 @@ package com.example.proyectofinaluxmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.ImageView
 
 class RegistrarseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrarse)
+
+        val loginButtonRegistrarse: Button = findViewById(R.id.loginButtonRegistrarse)
+
+        loginButtonRegistrarse.setOnClickListener {
+            val intent = Intent(this, TerminosActivity::class.java)
+            startActivity(intent)
+        }
+
+        val backButtonRegistrarse: ImageView = findViewById(R.id.backButtonRegistrarse)
+
+        backButtonRegistrarse.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/proyectofinaluxmobile/TerminosActivity.kt
+++ app/src/main/java/com/example/proyectofinaluxmobile/TerminosActivity.kt
@@ -2,10 +2,27 @@ package com.example.proyectofinaluxmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.ImageView
 
 class TerminosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terminos)
+
+        val aceptarTerminosButton: Button = findViewById(R.id.aceptarTerminosButton)
+
+        aceptarTerminosButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val backButtonTerminos: ImageView = findViewById(R.id.backButtonTerminos)
+
+        backButtonTerminos.setOnClickListener {
+            val intent = Intent(this, RegistrarseActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

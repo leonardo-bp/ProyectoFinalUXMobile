538aa9df93e353d1d0f49c2193b6fe9240da7653
Leonardo Barrios Poveda <o.barriosp@uniandes.edu.co>
2024-03-08 21:53:36 -0500

    Se finalizan interacciones entre pantallas inicio y main

--- app/src/main/java/com/example/proyectofinaluxmobile/InicioActivity.kt
+++ app/src/main/java/com/example/proyectofinaluxmobile/InicioActivity.kt
@@ -1,11 +1,20 @@
 package com.example.proyectofinaluxmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+        val cerrarSesionButtonInicio: Button = findViewById(R.id.cerrarSesionButtonInicio)
+
+        cerrarSesionButtonInicio.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/proyectofinaluxmobile/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinaluxmobile/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.proyectofinaluxmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val registerButton: Button = findViewById(R.id.registerButtonMain)
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegistrarseActivity::class.java)
+            startActivity(intent)
+        }
+
+        val loginButtonMain: Button = findViewById(R.id.loginButtonMain)
+
+        loginButtonMain.setOnClickListener {
+            val intent = Intent(this, InicioActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

b7239736aa8a0a71ddb8f195d2e8a5b757de1e1d
IngSneyderAmado <s.amadov@uniandes.edu.co>
2024-03-12 19:58:28 -0500

    se crea pantalla de conexion betplay

---
+++ app/src/main/java/com/example/proyectofinaluxmobile/BetPlayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyectofinaluxmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BetPlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bet_play)
+    }
+}
